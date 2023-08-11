0d4ec43f395fb03e31fb2cc6646716caa1cffa44
angelo ayranji <angeloayranji@gmail.com>
2023-08-12 02:36:58 +0300

    small changes

--- src/services/classifier.service.ts
+++ src/services/classifier.service.ts
@@ -13,13 +13,10 @@ export class ClassifierService {
         const fairPlatformsAddresses: string[] = await this.getPlatformsAddresses(true)
         const unfairPlatformsAddresses: string[] = await this.getPlatformsAddresses(false)
 
-        const fairQuery = this.buildQuery(fairPlatformsAddresses, tokenId)
-        const unfairQuery = this.buildQuery(unfairPlatformsAddresses, tokenId)
+        const fairCount: number = await this.fairnessCount(true, fairPlatformsAddresses, tokenId)
+        const unfairCount: number = await this.fairnessCount(false, unfairPlatformsAddresses, tokenId)
 
-        const fairCount: number = await this.fairnessCount(true, fairQuery)
-        const unfairCount: number = await this.fairnessCount(false, unfairQuery)
-
-        return this.calculateScore(5, 5)
+        return this.calculateScore(fairCount, unfairCount)
     }
 
     private async getPlatformsAddresses(fair: boolean = true): Promise<string[]> {
@@ -29,6 +26,19 @@ export class ClassifierService {
         return platformsAddresses
     }
 
+
+    private async fairnessCount(fair: boolean = true, platformAddresses: string[], tokenId: number): Promise<number> {
+        const query = this.buildQuery(platformAddresses, tokenId)
+
+        const res = await axios.post(process.env.SUBGRAPH_URL ? process.env.SUBGRAPH_URL : "", {
+            query: query
+        })
+
+        console.log(res.data.data.transfers)
+        
+        return res.data.data.transfers.length
+    }
+
     private buildQuery(platformAddresses: string[], tokenId: number): string {
         return `query ExampleQuery {
             transfers (where: { to_in: ${JSON.stringify(platformAddresses)}, tokenId: "${tokenId}"}) {
@@ -36,17 +46,6 @@ export class ClassifierService {
               tokenId
             }
           }`
-    }
-
-    private async fairnessCount(fair: boolean = true, query: string): Promise<number> {
-        const res = await axios.post(process.env.SUBGRAPH_URL ? process.env.SUBGRAPH_URL : "", {
-            query: query
-        })
-
-        console.log(query)
-        console.log(res.data.data.transfers)
-        
-        return 5
     }
 
     private calculateScore(fairCount: number, unfairCount: number): number {
